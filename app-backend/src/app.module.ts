316ab7ea7360a873e391922092ae0aa7d815d397
Aaron Hawro <aaronhawro@gmail.com>
2025-03-04 10:28:46 +0100

    link db with code (not tested)

--- app-backend/src/app.module.ts
+++ app-backend/src/app.module.ts
@@ -2,8 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import * as dotenv from 'dotenv';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+dotenv.config();
+
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT!, 10), // the ! is necessary, assures typeScript that its a valid data type despite not being a correct data type
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true, // !!ONLY FOR TIME OF DEVELOPMENT!!
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
