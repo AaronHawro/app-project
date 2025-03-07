f5163bc2cf09036f3c7afdf6f3fdd57916aea905
Aaron Hawro <aaronhawro@gmail.com>
2025-03-07 14:24:15 +0100

    module fix

--- app-frontend/src/app/app.module.ts
+++ app-frontend/src/app/app.module.ts
@@ -2,11 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { MatIconModule } from '@angular/material/icon';
 import { provideHttpClient } from '@angular/common/http';
 import { MainPageComponent } from './main-page/main-page.component';
-import { ProjectViewComponent } from './project-view/project-view.component';
 import { ProjectEditComponent } from './project/project-edit/project-edit.component';
 import { TeamViewComponent } from './team/team-view/team-view.component';
 import { TeamEditComponent } from './team/team-edit/team-edit.component';
@@ -15,10 +13,10 @@ import { UserEditComponent } from './user/user-edit/user-edit.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { TaskEditComponent } from './task/task-edit/task-edit.component';
 import { TaskViewComponent } from './task/task-view/task-view.component';
+import { ProjectViewComponent } from './project/project-view/project-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
     MainPageComponent,
     ProjectViewComponent,
     ProjectEditComponent,
@@ -39,6 +37,6 @@ import { TaskViewComponent } from './task/task-view/task-view.component';
     provideClientHydration(withEventReplay()),
     provideHttpClient()
   ],
-  bootstrap: [AppComponent]
+  bootstrap: []
 })
 export class AppModule { }
